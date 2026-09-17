60ee3dd17bfa159b6cd1ef8ca687c3c7e1c86933
phuthanet035 <674230035@webmail.npru.ac.th>
2026-09-17 20:04:14 +0700

    Delete php035-036-main/php035-036-main directory

--- php035-036-main/php035-036-main/miniproject/ServiceInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Paper.php';
-
-interface ServiceInterface {
-    public function calculatePrice(Paper $paper, int $pages, int $quantity): float;
-    public function getName(): string;
-}
